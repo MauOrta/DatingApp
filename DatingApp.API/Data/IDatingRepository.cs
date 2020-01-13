d3725b2623ddb63499bcf3cd9e561ab46a6b22ad
mauricio ivan orta nuñez <zmaut@hotmail.com>
2020-01-13 14:26:40 -0600

    End of Section 16

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -16,5 +16,8 @@ namespace DatingApp.API.Data
          
          Task<Photo> GetMainPhotoForUSer(int userId);
          Task<Like> GetLike(int userId, int recipientId);
+         Task<Message> GetMessages(int id);
+         Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams);
+         Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
     }
 }
